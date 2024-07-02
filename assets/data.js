35ac1050de4ee6d11bbdf8939f7f556554bbcfd8
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2024-07-02 20:04:14 +0000

    fix: Use redirect URL for Resume

--- assets/data.js
+++ assets/data.js
@@ -3,7 +3,7 @@ export const bioData = {
   description:
     'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
   resume:
-    'https://drive.google.com/file/d/1xSuVUMK4WzzzA088TGtvc_zGQbBhhwHe/view',
+    '/CV',
   linkedin: 'https://www.linkedin.com/in/kunalragrawal/',
   github: 'https://github.com/kunalagra/',
   mailto: 'mailto:kunalagrawal@tutanota.com',
